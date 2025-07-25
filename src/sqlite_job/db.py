94fa23915438a0b59f00fc666e7a07f705fec0fb
Aakash Bakhle <48802744+aakashb95@users.noreply.github.com>
2025-07-25 13:37:09 +0530

    db in cwd

--- src/sqlite_job/db.py
+++ src/sqlite_job/db.py
@@ -3,9 +3,7 @@ from contextlib import contextmanager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-import os
-db_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "sqlite_job.db")
-engine = create_engine(f"sqlite:///{db_path}")
+engine = create_engine("sqlite:///sqlite_job.db")
 
 
 def get_db():
